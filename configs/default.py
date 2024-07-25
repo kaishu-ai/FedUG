e31638e9efbede483f25dc295ac8557cd494217a
kai shu <kaishu.cs@gmail.com>
2024-07-25 21:42:15 +0800

    temp

---
+++ configs/default.py
@@ -0,0 +1,9 @@
+pacs_domain_list = ['p', 'a', 'c', 's']
+domainNet_domain_list = ['c', 'i', 'p', 'q', 'r', 's']
+officehome_domain_list = ['p', 'a', 'c', 'r']
+terra_incognita_list = ['100', '38', '43', '46']
+
+log_count_path = '/home/ubuntu/data/pycharm/FedUG/logs'
+pacs_path = '/home/ubuntu/data/pycharm/FedUG/PACS_DataSet'
+officehome_path = '/home/ubuntu/data/pycharm/FedUG/OfficeHomeDataset_10072016'
+terrainc_path = ''
