567fa127aabbedb7bee03642d994461aaa90e594
kai shu <kaishu.cs@gmail.com>
2024-07-25 21:40:11 +0800

    temp

--- configs/default.py
+++
@@ -1,9 +0,0 @@
-pacs_domain_list = ['p', 'a', 'c', 's']
-domainNet_domain_list = ['c', 'i', 'p', 'q', 'r', 's']
-officehome_domain_list = ['p', 'a', 'c', 'r']
-terra_incognita_list = ['100', '38', '43', '46']
-
-log_path = ''
-pacs_path = ''
-officehome_path = ''
-terrainc_path = ''
